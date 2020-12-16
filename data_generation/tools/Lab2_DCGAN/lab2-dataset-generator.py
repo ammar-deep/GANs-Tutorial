ec84e7d7df034e0840894cab9a62fa71b163b836
ammar-deep <76093480+ammar-deep@users.noreply.github.com>
2020-12-16 17:31:38 +0900

    Added files for tutorial
    These are the jupyter notebooks and other files for the tutorial.

---
+++ data_generation/tools/Lab2_DCGAN/lab2-dataset-generator.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import argparse
+import glob
+import io
+import os
+import random
+
+import numpy
+from PIL import Image, ImageFont, ImageDraw
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Default data paths.
+DEFAULT_LABEL_FILE = os.path.join(SCRIPT_PATH,
+                                  '../labels/alphabets.txt')
+DEFAULT_FONTS_DIR = os.path.join(SCRIPT_PATH, '../fonts')
+DEFAULT_OUTPUT_DIR = os.path.join(SCRIPT_PATH, '../../Lab2-image-data')
+
+# Width and height of the resulting image.
+IMAGE_WIDTH = 64
+IMAGE_HEIGHT = 64
+
+def generate_hangul_images(label_file, fonts_dir, output_dir):
+    """Generate Hangul image files.
+
+    This will take in the passed in labels file and will generate several
+    images using the font files provided in the font directory. The font
+    directory is expected to be populated with *.ttf (True Type Font) files.
+    The generated images will be stored in the given output directory. Image
+    paths will have their corresponding labels listed in a CSV file.
+    """
+    with io.open(label_file, 'r', encoding='utf-8') as f:
+        labels = f.read().splitlines()
+
+    image_dir = os.path.join(output_dir, 'font-images')
+    if not os.path.exists(image_dir):
+        os.makedirs(os.path.join(image_dir))
+
+    # Get a list of the fonts.
+    fonts = glob.glob(os.path.join(fonts_dir, '*.ttf'))
+
+    total_count = 0
+    prev_count = 0
+    for character in labels:
+        # Print image count roughly every 5000 images.
+        if total_count - prev_count > 5000:
+            prev_count = total_count
+            print('{} images generated...'.format(total_count))
+
+        for font in fonts:
+            total_count += 1
+            image = Image.new('L', (IMAGE_WIDTH, IMAGE_HEIGHT), color=0)
+            font = ImageFont.truetype(font, 48)
+            drawing = ImageDraw.Draw(image)
+            w, h = drawing.textsize(character, font=font)
+            drawing.text(
+                ((IMAGE_WIDTH-w)/2, (IMAGE_HEIGHT-h)/2),
+                character,
+                fill=(255),
+                font=font
+            )
+            file_string = '{}.png'.format(total_count)
+            file_path = os.path.join(image_dir, file_string)
+            image.save(file_path, 'PNG')
+
+    print('Finished generating {} images.'.format(total_count))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--label-file', type=str, dest='label_file',
+                        default=DEFAULT_LABEL_FILE,
+                        help='File containing newline delimited labels.')
+    parser.add_argument('--font-dir', type=str, dest='fonts_dir',
+                        default=DEFAULT_FONTS_DIR,
+                        help='Directory of ttf fonts to use.')
+    parser.add_argument('--output-dir', type=str, dest='output_dir',
+                        default=DEFAULT_OUTPUT_DIR,
+                        help='Output directory to store generated images and '
+                             'label CSV file.')
+    args = parser.parse_args()
+    generate_hangul_images(args.label_file, args.fonts_dir, args.output_dir)
